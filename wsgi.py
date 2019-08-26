ad995ad9f1d7186d1a3d978501b76f87b77ec45c
Alazar Genene <alazar.genene@gmail.com>
2019-08-26 16:07:38 -0400

    fixed some files

--- wsgi.py
+++ wsgi.py
@@ -7,10 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoproject.settings')
-
-application = get_wsgi_application()
+web: gunicorn myproject.wsgi
