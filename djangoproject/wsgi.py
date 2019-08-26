e29d556885a95d4bc2ae08831ec225b82eb13f7c
Alazar Genene <alazar.genene@gmail.com>
2019-08-26 18:12:29 -0400

    made some changes

--- djangoproject/wsgi.py
+++ djangoproject/wsgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-# import os
+import os
 
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
-web: gunicorn myproject.wsgi
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoproject.settings')
+
+application = get_wsgi_application()
